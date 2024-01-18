6893beef6d8906c77ca9bde20d270f455baa6225
Sanjog34 <108666448+Sanjog34@users.noreply.github.com>
2024-01-18 18:12:53 +0545

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,31 +1,46 @@
 #include <alpha.h>
+
 const int dataPin = 2;
 const int clockPin = 4;
 const int latchPin = 3;
-
+const int arraySize=8;
+const int numArrays=50;
+int hold,temp[8]={};
 void setup() {
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  DISP(S);
-  DISP(A);
-  DISP(N);
-  DISP(J);
-  DISP(O);
-  DISP(G);
-}
-void updateShiftRegister(byte data){
-digitalWrite(latchPin,LOW);
-shiftWrite(dataPin,clockPin,LSBFIRST,data);
+ const char** stringArrays[numArrays] = {S,A,N,J,O,G,_,S,A,P,K,O,T,A,_};
+  convert(stringArrays);    
+}  
+
+
+void updateShiftRegister(byte data) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, data); 
 }
 
 void DISP(int alpha[]){
     long int instant=millis();
-  while(millis()-instant<=500){//display each character for 500ms
-  for (int i = 0; i < 8; i++) {//display one out of 8 rows at a time. Doing it sufficiently fast gives a picture of character in the matrix.
+  while(millis()-instant<=500){
+  for (int i = 0; i < 8; i++) {
     updateShiftRegister(128>>i);
     updateShiftRegister(~(alpha[i]));
     digitalWrite(latchPin, HIGH); 
     }
    }
 }
+void convert(char** stringArrays[numArrays]){
+  for (int i = 0; stringArrays[i]!=NULL; ++i) {
+    for (int j = 0; j < arraySize; ++j) {
+        hold=atoi(stringArrays[i][j]);
+        temp[j]=hold;
+//      Serial.println(stringArrays[i][j]);
+    }
+    DISP(temp);
+  }
+  }
